ab934f0bc2da378ac2f0838a67d27ab877f59a1f
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2017-11-20 09:12:45 +0000

    Update slurm_install_nodes.sh

--- HPC/scripts/slurm-install/nodes/slurm_install_nodes.sh
+++ HPC/scripts/slurm-install/nodes/slurm_install_nodes.sh
@@ -5,9 +5,10 @@
 
 yum -y install openssl openssl-devel pam-devel numactl numactl-devel hwloc hwloc-devel lua lua-devel readline-devel rrdtool-devel ncurses-devel man2html libibmad libibumad perl-ExtUtils-MakeMaker
 
+# /share must be mounted on nodes from master's NFS (as should be after normal deployment)
 cd /share/installs/slurm/
 
-# change to your slurm vesion below (see rpms compiled in 1st step)
+# change to your slurm version below (see rpms compiled in 1st step)
 
 rpm -Uvh slurm-17.02.9.el7.centos.x86_64.rpm slurm-sjobexit-17.02.9.el7.centos.x86_64.rpm  slurm-devel-17.02.9.el7.centos.x86_64.rpm  slurm-perlapi-17.02.9.el7.centos.x86_64.rpm slurm-sjstat-17.02.9.el7.centos.x86_64.rpm  slurm-sql-17.02.9.el7.centos.x86_64.rpm slurm-munge-17.02.9.el7.centos.x86_64.rpm  slurm-plugins-17.02.9.el7.centos.x86_64.rpm slurm-slurmdbd-17.02.9.el7.centos.x86_64.rpm
 
@@ -15,7 +16,7 @@ rpm -Uvh slurm-17.02.9.el7.centos.x86_64.rpm slurm-sjobexit-17.02.9.el7.centos.x
 
 
 
-
+# For the reference, these are rpms we are installing on the nodes (command line above)
 #rpm -Uvh 
 #slurm
 #slurm-devel
